f0ca8da4650ca7188ea73840eedf2ae93e6f44c3
LAVANYA <lavanyach313@gmail.com>
2024-03-12 16:59:35 +0530

    first commit

--- assessment/src/index.js
+++ assessment/src/index.js
@@ -5,19 +5,19 @@ import Dashboard from './Dashboard';
 import ProductsManagement from './ProductsManagement';
 import OrdersManagement from './OrdersManagement';
 import OrdersCalendarView from './OrdersCalendarView';
-import './style.css';
+import './index.css';
 
 const App = () => {
   return (
     <Router>
-        <div>
+      <div>
         <header>
           <h1 align="center">ERP System</h1>
           <h1 align="center">(Enterprise Resource Planning) System</h1>
-      
+          
           <nav>
             <ul>
-              <li><NavLink exact to="/" activeClassName="active" ><h2>Dashboard</h2></NavLink></li>
+              <li><NavLink exact to="/" activeClassName="active"><h2>Dashboard</h2></NavLink></li>
               <li><NavLink to="/products" activeClassName="active"><h2>Products Management</h2></NavLink></li>
               <li><NavLink to="/orders" activeClassName="active"><h2>Orders Management</h2></NavLink></li>
               <li><NavLink to="/calendar" activeClassName="active"><h2>Order Calendar View</h2></NavLink></li>
@@ -25,17 +25,15 @@ const App = () => {
           </nav>
         </header>
         <main>
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/products" component={ProductsManagement} />
-      <Route path="/orders" component={OrdersManagement} />
-      <Route path="/calendar" component={OrdersCalendarView} />
-    </Switch>
-  </main>
-        
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/products" component={ProductsManagement} />
+            <Route path="/orders" component={OrdersManagement} />
+            <Route path="/calendar" component={OrdersCalendarView} />
+          </Switch>
+        </main>
       </div>
     </Router>
-    
   );
 };
 
